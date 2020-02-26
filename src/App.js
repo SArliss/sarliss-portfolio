d62b527d1e6460b7023c35cbcc99941502b87020
sarliss <sinaramartins@gmail.com>
2020-02-25 21:56:40 -0500

    React boilerplate

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+
+
+    render(){
+      return (
+        <div className="App">
+
+          <header className="App-header">
+          </header>
+
+          <p>Sinara</p>
+
+        </div>
+      )
+    }
+}
+
+export default App;
