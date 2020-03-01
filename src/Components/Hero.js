3571ec237b9909a6566e60f552ffcb7c11549778
sarliss <sinaramartins@gmail.com>
2020-03-01 16:43:20 -0500

    Added down arrow to hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import downArrow from '../Images/downarrow.png'
 
 function Hero() {
   return (
     <div className="hero-section">
+
       <a href="/#about" className="nav-item">
-      <header>
-        <h1>Sinara Arliss</h1>
-        <h2>Software Engineer</h2>
+        <header>
+          <h1>Sinara Arliss</h1>
+          <h2>Software Engineer</h2>
         </header>
       </a>
+
+      <div className="arrow-section">
+        <a href="/#about">
+          <img className="down-arrow-img bounce" src={downArrow} />
+        </a>
+      </div>
+
     </div>
   )
 }
