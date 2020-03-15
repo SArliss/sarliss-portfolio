fd293466efdf97976ac3c30ff4b8bd79b46b78fa
sarliss <sinaramartins@gmail.com>
2020-03-14 23:56:24 -0400

    Updated about me summary and css line-height for global app font

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,20 +8,20 @@ function About() {
         <h3>About Me</h3>
         <section id="about-sinara">
 
-        <div className="about-sinara-photo-wrapper">
-          <img src={Sinara} id="sinara-photo" alt="Sinara avatar" />
+          <div className="about-sinara-photo-wrapper">
+            <img src={Sinara} id="sinara-photo" alt="Sinara avatar" />
           </div>
-          
+
           <div className="about-sinara-summary">
             <p>
-              Hello, my name is Sinara. I am a curious and self-driven software engineer,
-              who loves problem solving and the process of converting pseudocode into fully functional web applications.
-          </p>
+              Hello, my name is Sinara. I am a curious and self-driven Software Engineer with a Master’s in Computer Science,
+              and over five years of experience in the Information Technology field.
+            </p>
             <p>
-              With a Master’s in Computer Science and over five years of experience in the Information Technology field,
-              I am passionate about coding and I am continuously striving to acquire new skills.
-              I love being a developer because I can extend my creativity and passion on developing eloquently designed applications.
-          </p>
+              I am truly passionate about coding, I enjoy problem-solving and I am continuously striving to acquire new skills.
+              I love being a developer because I can combine my creativity and analytical traits
+              to develop beautifully designed and functional web applications.
+            </p>
             <p>
               I have lived abroad and I speak English and Portuguese fluently. I enjoy traveling and learning about new cultures,
               new languages and new ways of thinking. It has made me a highly empathetic and open-minded individual,
