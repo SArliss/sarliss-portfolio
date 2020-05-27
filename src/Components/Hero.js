acb5360430bb3d287353f8a26df272a780aa29e6
sarliss <sinaramartins@gmail.com>
2020-05-27 02:32:23 -0400

    Changed hero background, added animation

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import downArrow from '../Images/downarrow.png'
 
 function Hero() {
   return (
     <div className="hero-section">
 
       <a href="/#about">
-        <header>
+        <div className="hero-title">
           <h1>Sinara Arliss</h1>
           <h2>Software Engineer</h2>
-        </header>
+        </div>
       </a>
 
       <div className="arrow-section">
         <a href="/#about">
-          <img className="down-arrow-img bounce" src={downArrow} alt="arrow link"/>
+          <svg className="down-arrow-img bounce" width="100%" height="80px" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M 100 150 L 300 150 L 200 300 z"
+              fill="none"
+              stroke="white"
+              strokeWidth="4"
+            />
+          </svg>
         </a>
       </div>
 
