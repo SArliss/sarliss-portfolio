24a90fbbedaa8bb82b28d729db3cf3f82ca96438
sarliss <sinaramartins@gmail.com>
2020-05-28 03:01:58 -0400

    Updated resume link

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -12,7 +12,7 @@ function Resume() {
         </div>
 
         <div>
-          <a href="https://drive.google.com/file/d/1xLfzBWW-vBhPd5UGvvEN9S3KRkRCOzEB/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/1YiTqkkIKQZ-vsuaFvFpYUnnVbs5oJqKh/view?usp=sharing" target="_blank" rel="noopener noreferrer">
             <img src={File} id="resume-icon" alt="Sinara resume" />
           </a>
         </div>
