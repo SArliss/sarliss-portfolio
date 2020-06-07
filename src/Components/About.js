0d70ae5a1ebc94f6a9290086ee66b0b86a209b06
sarliss <sinaramartins@gmail.com>
2020-06-07 04:37:00 -0400

    Updated about me and footer sections

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,12 +18,13 @@ function About() {
             
             <p>Hello world,</p>
              
-            <p>I'm Sinara Arliss, a self-driven Software Engineer with a Master’s in Computer Science and over five years of work experience in the Information Technology industry.</p>
+            <p>I'm Sinara Arliss, a self-driven software engineer with a master’s in computer science and over five years of work experience in the information technology industry.</p>
             
-            <p>I enjoy problem-solving and I love being a Software Engineer because I can combine my creativity and analytical traits to find solutions to complex problems efficiently.
-            I'm passionate about coding, innovative technologies and I believe that creating eloquently designed web applications can be an enormous driver in improving and growing an organization.</p>
+            <p> I see myself as a full-stack web developer with a strength in the front-end and I'm passionate about clean code and great UI/UX. I love being a developer because I can combine my creativity and analytical traits to break down complex problems and solve them efficiently. 
+            I develop fully responsive web applications, and write high-quality code that is scalable and maintainable.
+            I believe that creating eloquently designed tools and applications can be an enormous driver in improving and growing an organization.</p>
 
-            <p>I live in New Jersey, USA, near the beatiful Navesink river and I have easy access to the Highlands-NYC Seastreak ferry. I was born in Brazil and I speak English and Portuguese fluently. I enjoy traveling and learning about new cultures, new languages and new ways of thinking. It has made me a highly empathetic and open-minded individual,
+            <p>I live in New Jersey, USA, near the beatiful Navesink river and I have easy access to the Highlands-NYC Seastreak ferry. I was born in Brazil and I speak English and Portuguese fluently. I enjoy traveling and learning about new cultures, new languages and new ways of thinking. It has made me an empathetic and open-minded individual,
               who can quickly adapt to changes in the work environment.</p>
 
             <p>I'm excited to connect with companies and colleagues that inspire me!</p>
